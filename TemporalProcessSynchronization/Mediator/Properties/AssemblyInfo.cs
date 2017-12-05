b54f7cbd748dabba1973eb8b2f06fa39b12ec82b
Bruno Blažeka <blazekab@gmail.com>
2017-12-05 22:28:47 +0100

    code adapted to a rabbitmq example, but it doesn't work

--- TemporalProcessSynchronization/Mediator/Properties/AssemblyInfo.cs
+++ TemporalProcessSynchronization/Mediator/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TemporalProcessSynchronization")]
+[assembly: AssemblyTitle("Mediator")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TemporalProcessSynchronization")]
+[assembly: AssemblyProduct("Mediator")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("815b4fc5-22e0-4b71-ac33-3f0b1cc7cfa5")]
+[assembly: Guid("5cc6dd01-1744-47ca-aa74-23650eebb242")]
 
 // Version information for an assembly consists of the following four values:
 //
